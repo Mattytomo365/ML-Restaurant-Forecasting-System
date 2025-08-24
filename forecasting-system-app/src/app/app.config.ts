e6f928fafe9675b6e09b7eccda9ca1f861fc9dd9
Mattytomo365 <matty.tom@icloud.com>
2025-08-24 18:56:19 +0100

    Firebase setup in Firebase console and Angular app (closes #95)

--- forecasting-system-app/src/app/app.config.ts
+++ forecasting-system-app/src/app/app.config.ts
@@ -3,10 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+import { firebaseConfig } from './firebase.config';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+
+    provideFirebaseApp(() => initializeApp(firebaseConfig)) // Calls Firebase Web SDK to initialise the Firebase App,
+    // wrapped within an Angular provider to register the Firebase App instance in Angular's DI container
   ]
 };
+
