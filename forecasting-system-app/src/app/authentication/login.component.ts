5afbd4777ca89c8a8b90906a70807c4b901e7ec6
Mattytomo365 <matty.tom@icloud.com>
2025-08-26 19:45:27 +0100

    Login component fully implemented (closes #94)

--- forecasting-system-app/src/app/authentication/login.component.ts
+++ forecasting-system-app/src/app/authentication/login.component.ts
@@ -1,11 +1,27 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-login.component',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,6 +31,8 @@ export class LoginComponent {
   private fb = inject(FormBuilder);
 
   error: string | null = null;
+  loading = false;
+  hide = true;
 
   form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
@@ -22,11 +40,16 @@ export class LoginComponent {
   });
 
   onLogin(){
+    this.error = null;
+    this.loading = true;
     const {email, password} = this.form.value;
     this.auth.login(email!, password!)
     .then(() => this.router.navigate(['/forecast'])) // .then returns a new Promise object, allowing for method chaining // user is navigated to forecasting page upon successful login
     .catch(err => {
-      this.error = err.code;
+      this.error = 'Invalid email or password';
+    })
+    .finally(() => {
+      this.loading = false // Ensures the variable resets even if an error occurs
     })
   }
 
