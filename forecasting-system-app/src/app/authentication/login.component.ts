98fc856994ed7ca3adb48a0e656dee8f0614e8ca
Mattytomo365 <matty.tom@icloud.com>
2025-08-26 15:52:47 +0100

    Login component logic implemented (#94)

--- forecasting-system-app/src/app/authentication/login.component.ts
+++ forecasting-system-app/src/app/authentication/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login.component',
@@ -7,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  error: string | null = null;
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  onLogin(){
+    const {email, password} = this.form.value;
+    this.auth.login(email!, password!)
+    .then(() => this.router.navigate(['/forecast'])) // .then returns a new Promise object, allowing for method chaining // user is navigated to forecasting page upon successful login
+    .catch(err => {
+      this.error = err.code;
+    })
+  }
+
 
 }
