e703efa4394d9ea44b268a7f9fcfb94e8053487f
Mattytomo365 <matty.tom@icloud.com>
2025-08-27 14:33:12 +0100

    HttpClient interceptor implemented & registered (closes #93)

--- forecasting-system-app/src/app/app.config.ts
+++ forecasting-system-app/src/app/app.config.ts
@@ -7,6 +7,8 @@ import { firebaseConfig } from './firebase.config';
 import { provideFirebaseApp } from '@angular/fire/app';
 import { initializeApp } from 'firebase/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './authentication/auth.interceptor-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [ // Providers are collections of tokens facilitating object creation and ensure singleton instances. They are injected into constructors, defining how dependencies are resolved, registration recipe
@@ -16,6 +18,8 @@ export const appConfig: ApplicationConfig = {
 
     provideFirebaseApp(() => initializeApp(firebaseConfig)), // Calls Firebase Web SDK to initialise the Firebase App
     provideAuth(() => getAuth()), // Returns the Firebase Authentication service tied to the initialised app
+
+    provideHttpClient(withInterceptors([authInterceptor])), // Registers HttpClient and the interceptor function its requests will flow through
   ]
 };
 
