5e550841f0dd59fc08535d3cf48f3197448373bd
Mattytomo365 <matty.tom@icloud.com>
2025-08-27 14:31:55 +0100

    Authentication route guard implemented and utilised (closes #99)

--- forecasting-system-app/src/app/app.routes.ts
+++ forecasting-system-app/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './authentication/login.component';
 import { ForecastComponent } from './forecast/forecast.component';
+import { authGuard } from './authentication/auth.guard-guard';
 
 export const routes: Routes = [
     {path: 'login', component: LoginComponent},
-    {path: 'forecast', component: ForecastComponent},
+
+    {
+        path: 'forecast', 
+        component: ForecastComponent,
+        canActivate: [authGuard]
+    },
+
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: '**', redirectTo: 'login'}
 ];
